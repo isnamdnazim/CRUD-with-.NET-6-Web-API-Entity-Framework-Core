278f7871409704ea31c6e02780a92bcea1550f61
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-20 21:18:57 +0600

    Working on get and post API method

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heros = new List<SuperHero> {
+                 new SuperHero {
+                     Id = 1,
+                     Name="Nazim Uddin",
+                     FirstName="Nazim",
+                     LastName="Uddin",
+                     Place="Dhaka"
+                 }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heros);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heros.Add(hero);
+            return Ok(heros);
+        }
+    }
+}
--- SuperHeroAPI/Controllers/WeatherForecastController.cs
+++ SuperHeroAPI/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace SuperHeroAPI.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,7 +18,7 @@ namespace SuperHeroAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("weather1")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
