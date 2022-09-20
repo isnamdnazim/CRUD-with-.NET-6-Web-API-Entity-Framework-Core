d9f2d57848542910bc0577bb63bba508a92a27e0
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-21 00:59:17 +0600

    Implemented API for get put post and delete

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -14,6 +14,13 @@ namespace SuperHeroAPI.Controllers
                      FirstName="Nazim",
                      LastName="Uddin",
                      Place="Dhaka"
+                 },
+                  new SuperHero {
+                     Id = 2,
+                     Name="Emon Shah",
+                     FirstName="Emon",
+                     LastName="Shah",
+                     Place="Kurigram"
                  }
             };
 
@@ -23,10 +30,49 @@ namespace SuperHeroAPI.Controllers
             return Ok(heros);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heros.Find(h => h.Id == id);
+            if (hero == null)
+            {
+                return BadRequest("Hero Not Found."); 
+            }
+                
+            return Ok(hero);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
             heros.Add(hero);
+            return Ok(heros);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heros.Find(h => h.Id == request.Id);
+            if (hero == null)
+            {
+                return BadRequest("Hero Not Found.");
+            }
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+            return Ok(heros);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Delete(int id)
+        {
+            var hero = heros.Find(h => h.Id == id);
+            if (hero == null)
+            {
+                return BadRequest("Hero Not Found.");
+            }
+            heros.Remove(hero);
             return Ok(heros);
         }
     }
