ff61c6f812007190171e27abb994681331382dba
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-27 00:02:32 +0600

    work done with Product CRUD API

--- SuperHeroAPI/Controllers/ProductController.cs
+++ SuperHeroAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace SuperHeroAPI.Controllers
 {
@@ -7,27 +8,76 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        public static List<Product> products = new List<Product> { 
-            new Product
-            {
-                Id = 1,
-                ProductName = "Apple",
-                ProductDescription = "Good Apple",
-                Price = 10
-            }
-        };
-        //private readonly DataContext _dataContext;
-        //public ProductController(DataContext dataContext)
-        //{
-        //    _dataContext = dataContext;
-        //}
+        //public static List<Product> products = new List<Product> { 
+        //    new Product
+        //    {
+        //        Id = 1,
+        //        ProductName = "Apple",
+        //        ProductDescription = "Good Apple",
+        //        Price = 10
+        //    }
+        //};
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> Get()
-        {
-            
-            return Ok(products);
+        { 
+            return Ok(await _dataContext.products.ToListAsync());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Product>>> Get(int id)
+        {
+            var product = await _dataContext.products.FindAsync(id);
+            if (product == null)
+            {
+                return BadRequest("Product Not Found");
+            }
+            return Ok(product);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+             _dataContext.products.Add(product);
+            await _dataContext.SaveChangesAsync();
+            return Ok(await _dataContext.products.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Product>>> UpdateProduct(Product product)
+        {
+            var dbProduct = await _dataContext.products.FindAsync(product.Id);
+            if(dbProduct == null)
+            {
+                return BadRequest("Product Not Found");
+            }
+            dbProduct.ProductName = product.ProductName;
+            dbProduct.ProductDescription = product.ProductDescription;
+            dbProduct.Price = product.Price;
+
+            await _dataContext.SaveChangesAsync();
+            return Ok( await _dataContext.products.ToListAsync()); 
+            
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Product>>> Delete (int id)
+        {
+            var product = await _dataContext.products.FindAsync(id);
+            if(product == null)
+            {
+                return BadRequest("Product Not Found");
+            }
+             _dataContext.products.Remove(product);
+            await _dataContext.SaveChangesAsync();
+            return Ok(await _dataContext.products.ToListAsync());
+        }
     }
 }
